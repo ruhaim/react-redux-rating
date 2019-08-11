919ad8edcfe8c981b248378669144ec3100e278e
ru iz <ruhaim@gmail.com>
2019-08-12 02:18:37 +0530

    pagination styling for bs4

--- src/Components/ReviewsList/Pagination.js
+++ src/Components/ReviewsList/Pagination.js
@@ -6,13 +6,19 @@ export const Pagination = (props)=>{
         <ReactPaginate
             previousLabel={'previous'}
             nextLabel={'next'}
-            breakLabel={'...'}
-            breakClassName={'break-me'}
+            breakLabel={<a className="page-link">...</a>}
+            breakClassName={'bpage-item'}
             marginPagesDisplayed={2}
             pageRangeDisplayed={5}
             containerClassName={'pagination'}
-            subContainerClassName={'pages pagination'}
+            subContainerClassName={'page-item'}
             activeClassName={'active'}
+            pageClassName="page-item"
+            previousClassName="page-item"
+            nextClassName="page-item"
+            pageLinkClassName="page-link"
+            previousLinkClassName="page-link"
+            nextLinkClassName="page-link"
             {...props}
         />)
 
